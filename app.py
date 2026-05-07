c5a74d0acb431b9c3bd24d948753687691b12690
Hrithik Kumar <158841783+goyalhrithik548@users.noreply.github.com>
2026-05-08 01:29:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
